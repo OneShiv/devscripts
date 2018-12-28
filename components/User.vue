c84e24f8fbb465c051edb77b30930626a26fc3e7
Shivshankar <shejwal1shivshankar@gmail.com>
2018-12-28 14:10:34 +0530

    first commit

---
+++ components/User.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <p>{{ firstName }}</p>
+         <p>{{ age }}</p>
+        <button @click="changeName">Click to change Name</button>
+        
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail 
+                :name="firstName" 
+                :usrAge="age"
+                @nameReset="firstName=$event"
+                :resetFn="resetName"
+                ></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit :userAge="age"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+  
+    export default {
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        },
+        data:function(){
+            return {
+                firstName:'Shivshankar',
+                age:27
+            }
+        },
+        methods:{
+            changeName(){
+                this.firstName='Pradnya';
+            },
+            resetName(){
+                this.firstName='abra ka dabra';
+            }    
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ components/UserDetail.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p> My name is {{switchName() }}</p>
+        <p> User Age {{ usrAge }}</p>
+        <button @click="resetName" >Reset</button>
+        <button @click="resetFn" >Reset Durs</button>
+    </div>
+</template>
+
+<script>
+  import { eventBus } from '../main';
+
+export default{
+    props:{
+        name:String,
+        resetFn:Function,
+        usrAge:Number
+    },
+    methods:{
+        switchName(){
+            return this.name.split("").reverse().join("");
+        },
+        resetName(){
+            this.name='hhhhhhhhhhhhhhhhh';
+            this.$emit('nameReset',this.name);
+        }
+    },
+    created(){
+            eventBus.$on('ageWasEdited',(age) => {
+                this.usrAge=age;
+            })
+        }
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
